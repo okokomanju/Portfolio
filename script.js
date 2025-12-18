fc569081c0b9607309a3da396440a8004523bb13
okoko <167289410+okokomanju@users.noreply.github.com>
2025-12-18 23:30:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("portfolio loaded");
+
+window.addEventListener("load", () => {
+    const catchText = document.querySelector(".catch-text");
+    setTimeout(() => {
+      catchText.classList.add("is-visible");
+    }, 800);
+  });
+
+  
